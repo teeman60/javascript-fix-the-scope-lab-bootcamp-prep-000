24426865c664e2512646738932e8e141d98e28b5
Taiye Salami <taiye41@gmail.com>
2019-05-13 10:01:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,10 +11,9 @@ function yourAnimal() {
 
 function add2(n) {
   var a = n + two
+const two = 2
 return n + two
-
-  const two = 2
-}
+  }
 
 var funkyFunction = function() {
   return function() {
