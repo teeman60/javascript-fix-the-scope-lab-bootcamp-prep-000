cb6659fb5305c0ddf4167cfe352de5273244991c
Taiye Salami <taiye41@gmail.com>
2019-05-13 10:10:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,10 +17,9 @@ return n + two
 
 var funkyFunction = function() {
   return function() {
+theFunk = "FUNKY!"
     return "FUNKY!"
   }
 }
 
-// We want to set theFunk equal to "FUNKY!" using our funkyFunction.
-// NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction
+
